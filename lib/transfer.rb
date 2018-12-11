6ebcc1fe63424390e34f38a470bf74e6d96c2131
Nick Inman <nick@theoatmeal.com>
2018-12-11 04:34:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
+require"pry"
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :transfer_amount
+  
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender 
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
+  end 
+  
+  def amount 
+    transfer_amount = 50
+  end 
+  
+  def valid?
+    #binding.pry
+    #sender.status == "open" && receiver.status == "open"
+    sender.valid? && receiver.valid? 
+  end 
+  
+  def execute_transaction
+    #binding.pry
+    1.times do
+      if valid?
+        sender.balance -= transfer_amount
+        receiver.balance += transfer_amount
+        @status = "complete"
+      else 
+        
+      end 
+    end
+  end 
 end
